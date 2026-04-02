b025439b3e19f611b62e2f63bfc3ecfe81cb54d4
Vladimir K <ew1abz@gmail.com>
2026-04-01 18:10:08 -0700

    Add 1/\dump_caps command to rotctld
    Returns the capability block that gpredict and other Hamlib clients
    probe on connect. Includes model metadata, supported operations, and
    current soft limits for AZ/EL range.

--- src/tasks/rotctld.rs
+++ src/tasks/rotctld.rs
@@ -113,6 +113,12 @@ pub async fn rotctld_task(stack: embassy_net::Stack<'static>) -> ! {
                             _        => { let _ = core::write!(resp, "RPRT -1\n"); }
                         }
                     }
+                } else if line == "1" || line == "\\dump_caps" {
+                    let lim = LIMITS.lock(|c| c.get());
+                    let _ = core::write!(resp,
+                        "Caps dump for model: 2\nModel name:\tPolar Pilot\nMfg name:\tCustom\nBackend version:\t{}\nBackend status:\tAlpha\nRotator type:\tAz-El\nCan set position:\tY\nCan get position:\tY\nCan stop:\tY\nCan reset:\tY\nCan move:\tY\nMin Azimuth:\t{:.2}\nMax Azimuth:\t{:.2}\nMin Elevation:\t{:.2}\nMax Elevation:\t{:.2}\nRPRT 0\n",
+                        env!("CARGO_PKG_VERSION"),
+                        lim.az_min, lim.az_max, lim.el_min, lim.el_max);
                 } else if line == "q" || line == "\\quit" {
                     break 'conn;
                 } else if line == "_" || line == "\\get_info" {
